fe744300136385579eab67f7ef6bb26205347872
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-10-07 10:00:02 +0530

    Create frequncy_count_ele_map.cpp

---
+++ frequncy_count_ele_map.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void countfrequnecy(int arr[],int n){
+  unordered_map<int, int> map;
+
+  for(int i=0;i<n;i++){
+    map[arr[i]]++;
+  }
+
+  for(auto x: map){
+    cout << x.first << " " << x.second << endl;
+  }
+}
+
+int main() {
+  int arr[] = {10,5,10,15,10,5};
+
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  countfrequnecy(arr,n);
+  return 0;
+}
